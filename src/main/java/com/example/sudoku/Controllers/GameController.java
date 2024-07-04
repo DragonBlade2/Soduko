fc97f292365db210fda816be2c3f3ca3c8daf413
DragonBlade2 <SavirKana@gmail.com>
2024-07-04 16:57:25 +0200

    sudoku game

--- src/main/java/com/example/sudoku/Controllers/GameController.java
+++ src/main/java/com/example/sudoku/Controllers/GameController.java
@@ -1,13 +1,17 @@
 package com.example.sudoku.Controllers;
 
+import com.example.sudoku.Board.BoardCreator;
 import com.example.sudoku.utils.FileUtils;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.FlowPane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import static com.example.sudoku.Board.BoardCreator.handleKeyPress;
 
 public class GameController implements Initializable {
 
@@ -15,11 +19,20 @@ public class GameController implements Initializable {
     private AnchorPane apContent;
 
     @FXML
-    private StackPane spBoard;
+    private FlowPane fpBoard;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        BoardCreator.createBoard(fpBoard, null);
+        fpBoard.setFocusTraversable(true);
+        fpBoard.requestFocus();
+    }
 
+    @FXML
+    void changeCellValue(KeyEvent event) {
+        if (BoardCreator.selectedRow != -1 && BoardCreator.selectedCol != -1) {
+            handleKeyPress(event);
+        }
     }
 
     @FXML
